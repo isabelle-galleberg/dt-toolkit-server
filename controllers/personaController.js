99daf72ed172f3b22492c7d16296241259a59d33
isabelle-galleberg <isabelle.galleberg@gmail.com>
2025-03-04 11:11:52 +0100

    fix: characteristics -> traits

--- controllers/personaController.js
+++ controllers/personaController.js
@@ -1,38 +1,38 @@
 const Persona = require("../models/Persona");
 
 exports.getPersona = async (req, res) => {
-  try {
-    const persona = await Persona.findOne({
-      userId: req.user.id,
-      cardId: req.params.cardId,
-    });
-    if (!persona) {
-      return res.status(200).json({
-        userId: req.user.id,
-        cardId: req.params.cardId,
-        name: "",
-        age: null,
-        occupationAndHobbies: "",
-        technologyUsage: "",
-        sliders: [0, 0, 0, 0, 0, 0],
-        characteristics: [],
-      });
-    }
-    res.status(200).json(persona);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+	try {
+		const persona = await Persona.findOne({
+			userId: req.user.id,
+			cardId: req.params.cardId,
+		});
+		if (!persona) {
+			return res.status(200).json({
+				userId: req.user.id,
+				cardId: req.params.cardId,
+				name: "",
+				age: null,
+				occupationAndHobbies: "",
+				technologyUsage: "",
+				sliders: [0, 0, 0, 0, 0, 0],
+				traits: [],
+			});
+		}
+		res.status(200).json(persona);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
 };
 
 exports.upsertPersona = async (req, res) => {
-  try {
-    const persona = await Persona.findOneAndUpdate(
-      { userId: req.user.id, cardId: req.body.cardId },
-      { ...req.body, userId: req.user.id },
-      { new: true, upsert: true }
-    );
-    res.status(200).json(persona);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+	try {
+		const persona = await Persona.findOneAndUpdate(
+			{ userId: req.user.id, cardId: req.body.cardId },
+			{ ...req.body, userId: req.user.id },
+			{ new: true, upsert: true }
+		);
+		res.status(200).json(persona);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
 };

bbd133d8bfc4c66ba585a9c3921042f809c50a36
Eva Teisberg <90642438+evateis@users.noreply.github.com>
2025-03-19 13:59:14 +0100

    feat: dynamic persona text (#14)
    Co-authored-by: evateis <eva.teisberg@soprasteria.com>

--- models/Story.js
+++ models/Story.js
@@ -1,18 +1,19 @@
 const mongoose = require("mongoose");
 
 const storySchema = new mongoose.Schema({
-	personaId: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "PersonaCard",
-		required: true,
-	},
-	storyline: { type: [[String]], required: true },
-	happyImg: { type: String, required: true },
-	sadImg: { type: String, required: true },
-	disappointedImg: { type: String, required: true },
-	surprisedImg: { type: String, required: true },
-	angryImg: { type: String, required: true },
-	scaredImg: { type: String, required: true },
+  personaId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "PersonaCard",
+    required: true,
+  },
+  introduction: { type: [String], required: true },
+  storyline: { type: [[String]], required: true },
+  happyImg: { type: String, required: true },
+  sadImg: { type: String, required: true },
+  disappointedImg: { type: String, required: true },
+  surprisedImg: { type: String, required: true },
+  angryImg: { type: String, required: true },
+  scaredImg: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Story", storySchema);

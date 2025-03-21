5b5f9215141ae96ed01763d30bad5a75f79594c1
Eva Teisberg <90642438+evateis@users.noreply.github.com>
2025-03-21 12:52:43 +0100

    feat: get and upsert feedback (#19)
    Co-authored-by: evateis <eva.teisberg@soprasteria.com>

---
+++ models/Feedback.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const feedbackSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  cardId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "PersonaCard",
+    required: true,
+  },
+  feedback: { type: [String], default: [] },
+});
+
+feedbackSchema.index({ userId: 1, cardId: 1 }, { unique: true });
+
+module.exports = mongoose.model("Feedback", feedbackSchema);

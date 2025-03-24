ee19a353b9e75306f15de54da8f4e6d5db184f40
Eva Teisberg <90642438+evateis@users.noreply.github.com>
2025-03-24 12:59:15 +0100

    feat: add fields for phone component (#21)
    Co-authored-by: evateis <eva.teisberg@soprasteria.com>

--- models/PersonaCard.js
+++ models/PersonaCard.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 
 const personaCardSchema = new mongoose.Schema({
-	alias: { type: String, required: true },
-	cardImageUrl: { type: String, required: true },
-	personaImageUrl: { type: String, required: true },
-	phoneImageUrl: { type: String, required: true },
-	traits: { type: Array, required: true },
+  alias: { type: String, required: true },
+  cardImageUrl: { type: String, required: true },
+  personaImageUrl: { type: String, required: true },
+  traits: { type: Array, required: true },
+  sender: { type: String },
+  subject: { type: String },
+  text: { type: [String] },
+  buttonText: { type: String },
+  buttonLink: { type: String },
 });
 
 module.exports = mongoose.model("PersonaCard", personaCardSchema);

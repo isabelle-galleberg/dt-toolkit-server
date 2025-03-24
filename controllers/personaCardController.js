a2c5387e2d580fad66a42baadf0342c8530e32d9
evateis <eva.teisberg@soprasteria.com>
2025-03-24 12:47:45 +0100

    feat: add fields for phone component

--- controllers/personaCardController.js
+++ controllers/personaCardController.js
@@ -1,29 +1,31 @@
 const PersonaCard = require("../models/PersonaCard");
 
 exports.getCard = async (req, res) => {
-	try {
-		const personas = await PersonaCard.find();
-		res.status(200).json(personas);
-	} catch (err) {
-		res.status(500).json({ message: err.message });
-	}
+  try {
+    const personas = await PersonaCard.find();
+    res.status(200).json(personas);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 exports.addCard = async (req, res) => {
-	const persona = new PersonaCard({
-		alias: req.body.alias,
-		cardImageUrl: req.body.cardImageUrl,
-		personaImageUrl: req.body.personaImageUrl,
-		phoneImageUrl: req.body.phoneImageUrl,
-		traits: req.body.traits,
-	});
+  const persona = new PersonaCard({
+    alias: req.body.alias,
+    cardImageUrl: req.body.cardImageUrl,
+    personaImageUrl: req.body.personaImageUrl,
+    traits: req.body.traits,
+    sender: req.body.sender,
+    subject: req.body.subject,
+    text: req.body.text,
+    buttonText: req.body.buttonText,
+    buttonLink: req.body.buttonLink,
+  });
 
-	console.log(persona);
-
-	try {
-		const newPersona = await persona.save();
-		res.status(201).json(newPersona);
-	} catch (err) {
-		res.status(400).json({ message: err.message });
-	}
+  try {
+    const newPersona = await persona.save();
+    res.status(201).json(newPersona);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 };

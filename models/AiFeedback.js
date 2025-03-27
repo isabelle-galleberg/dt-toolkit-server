94cbb79c38d2562d217521ff643b9e62f493cdb9
isabelle-galleberg <isabelle.galleberg@gmail.com>
2025-03-27 11:32:17 +0100

    fix: remove personaId

--- models/AiFeedback.js
+++ models/AiFeedback.js
@@ -1,20 +1,15 @@
 const mongoose = require("mongoose");
 
 const aiFeedbackSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  cardId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "PersonaCard",
-    required: true,
-  },
-  strengths: { type: String, default: "" },
-  improvements: { type: String, default: "" },
+	userId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
+	strengths: { type: String, default: "" },
+	improvements: { type: String, default: "" },
 });
 
-aiFeedbackSchema.index({ userId: 1, cardId: 1 }, { unique: true });
+aiFeedbackSchema.index({ userId: 1 }, { unique: true });
 
 module.exports = mongoose.model("AiFeedback", aiFeedbackSchema);
--- routes/aiFeedbackRoutes.js
+++ routes/aiFeedbackRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const {
-  getAiFeedback,
-  updateAiFeedback,
+	getAiFeedback,
+	updateAiFeedback,
 } = require("../controllers/aiFeedbackController");
 const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
 
-router.get("/:cardId", protect, getAiFeedback);
+router.get("/", protect, getAiFeedback);
 router.put("/", protect, updateAiFeedback);
 
 module.exports = router;
